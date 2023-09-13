479af0a71746440bf9a7d1c52e4591145d31b596
Pedro Oliveira <pdrhenriqe@outlook.com>
2023-09-13 09:45:45 -0300

    Mod9

---
+++ src/Mod9.java
@@ -0,0 +1,30 @@
+public class Mod9 {
+    public static void main(String[] args) {
+
+        System.out.println("Olá Tutores!");
+
+        long cel = 11940028922l;
+        System.out.println(cel);
+
+        Long cell = 11940028922L;
+        System.out.println(cell);
+
+        ///////////////////////////////////
+
+        int num = 123456789;
+
+        //casting explicito
+        System.out.println(num);
+
+        short num1 = (short) num;
+        System.out.println(num1);
+
+        //casting implicito
+        long num2 = num;
+        System.out.println(num2);
+
+
+
+
+    }
+}
